7f7b2eb5a43142a80dd138a2d0b119338de390c0
linyon <asd357076@gmail.com>
2017-05-09 22:47:03 +0800

    20170509-rgb轉hsi+直方圖均衡化

---
+++ Exercise/20170509-RGB_2_HSI_HistogramEqualization/20170509-RGB_2_HSI_HistogramEqualization/Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace _20170509_RGB_2_HSI_HistogramEqualization
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(22, 23);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(103, 52);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "開圖";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(22, 93);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(103, 52);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "RGB轉HSI";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(150, 23);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(411, 299);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(150, 328);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(805, 279);
+            this.chart1.TabIndex = 5;
+            this.chart1.Text = "chart1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1017, 619);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+    }
+}
+
---
+++ Exercise/20170509-RGB_2_HSI_HistogramEqualization/20170509-RGB_2_HSI_HistogramEqualization/Form1.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing.Imaging;
+namespace _20170509_RGB_2_HSI_HistogramEqualization
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Bitmap ori_image;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "Image Files(*.BMP;*.JPG;*.GIF;*.PNG)|*.BMP;*.JPG;*.GIF;*.PNG";
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                pictureBox1.Image = Image.FromFile(ofd.FileName);
+            }
+            ori_image = pictureBox1.Image as Bitmap;
+            pictureBox1.Image = ori_image;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int w = ori_image.Width;
+            int h = ori_image.Height;
+            var imageRect = new Rectangle(0, 0, w, h);
+            var ori_data = ori_image.LockBits(imageRect, ImageLockMode.ReadWrite, ori_image.PixelFormat);
+            var hsi_image = new Bitmap(imageRect.Width, imageRect.Height);
+            var hsi_data = hsi_image.LockBits(imageRect, ImageLockMode.ReadWrite, ori_image.PixelFormat);
+
+            System.IntPtr ori_Ptr = ori_data.Scan0;
+            System.IntPtr hsi_Ptr = hsi_data.Scan0;
+
+            int ori_bytes = ori_data.Stride * h;
+            int hsi_bytes = hsi_data.Stride * h;
+            byte[] ori_Values = new byte[ori_bytes];
+            byte[] hsi_Values = new byte[hsi_bytes];
+            System.Runtime.InteropServices.Marshal.Copy(ori_Ptr, ori_Values, 0, ori_bytes);//複製GRB信息到byte數組
+            System.Runtime.InteropServices.Marshal.Copy(hsi_Ptr, hsi_Values, 0, hsi_bytes);//複製GRB信息到byte數組
+            double radial, theta;
+            double tempD, tempB;
+            var hsi_h = new List<int>();
+            var hsi_s = new List<double>();
+            var hsi_i = new List<double>();
+            int i, j;
+            double r, g, b;
+            for (i = 0; i < h; i++) //RGB 轉 HSI
+            {
+                for (j = 0; j < w; j++)
+                {
+                    int ori_Index = i * ori_data.Stride + j * 3;
+                    var R = ori_Values[ori_Index + 2];
+                    var G = ori_Values[ori_Index + 1];
+                    var B = ori_Values[ori_Index];
+                    double level = 255.0;//R + G + B;
+                    r = (R / level);
+                    g = (G / level);
+                    b =(B / level);
+                    //H
+                    radial = Math.Acos(0.5 * ((r - g) + (r - b)) / Math.Sqrt((r - g) * (r - g) + (r - b) * (g - b)));
+                    theta = (radial * 180.0 / Math.PI);
+                    if (!double.IsNaN(theta)) tempD = (b <= g) ? theta:(360 - theta); 
+                    else tempD = 0;
+                    hsi_h.Add(Convert.ToInt32(tempD));
+                    //S
+                    tempB = Math.Min(r, g);
+                    tempB = Math.Min(tempB, b);
+                    tempD = 1.0 - 3.0 * tempB / (r + g + b);
+                    if (double.IsNaN(tempD)) tempD = 0;
+                    hsi_s.Add(tempD);
+                    //I
+                    tempD = (r + g + b) / 3.0;
+                    hsi_i.Add(tempD);
+                }
+            }
+            var hist = new int[256];
+            var fpHist = new int[256];
+            var eqHistTemp = new int[256];
+            var eqHist = new int[256];
+            var count_eq = new int[256];
+            int size = ori_bytes / 3;
+            int type;
+            // 統計每個灰階值出現的像素數量--------
+            for (i = 0; i < size; i++)
+            {
+                var tmp = (int)(hsi_i[i] * 255.0);
+                hist[tmp]++;
+            }
+            //計算累計直方圖-------------------
+            eqHistTemp[0] = hist[0];
+            for (i = 1; i < 256; i++)
+            {
+                eqHistTemp[i] = eqHistTemp[i - 1] + hist[i];
+            }
+            //累計分布並取整數，儲存計算出來的灰階值映射關係
+            for (i = 0; i < 256; i++)
+            {
+                eqHist[i] = (int)(255 * eqHistTemp[i] / size + 0.5);
+            }
+            //執行灰階值映射、均衡化
+            for (i = 0; i < size; i++)
+            {
+                var H = hsi_h[i];
+                if (H <= 120)
+                {
+                    type = 0;
+                }
+                else if (H > 120 && H <= 240)
+                {
+                    H = H - 120;
+                    type = 1;
+                }
+                else
+                {
+                    H = H - 240;
+                    type = 2;
+                }
+                theta = 60 - H;
+                var tmp_1 = ((1 - hsi_s[i])) / 3.0;
+                var tmp_2 = (1 + (hsi_s[i] * Math.Cos(H * Math.PI / 180.0) / Math.Cos(theta * Math.PI / 180.0))) / 3.0;
+                var tmp_3 = (1 - (tmp_1 + tmp_2));
+                if (type == 0)
+                {
+                    r = tmp_2;
+                    g = tmp_3;
+                    b = tmp_1;
+                }
+                else if (type == 1)
+                {
+                    r = tmp_1;
+                    g = tmp_2;
+                    b = tmp_3;
+                }
+                else
+                {
+                    r = tmp_3;
+                    g = tmp_1;
+                    b = tmp_2;
+                }        
+                var hsi_index = (int)(hsi_i[i] * 255.0);
+                r = r * 3.0 * eqHist[hsi_index];
+                g = g * 3.0 * eqHist[hsi_index];
+                b = b * 3.0 * eqHist[hsi_index];
+                if (r > 255)
+                    r = 255;
+                else if (r < 0)
+                    r = 0;
+                if (g > 255)
+                    g = 255;
+                else if (g < 0)
+                    g = 0;
+                if (b > 255)
+                    b = 255;
+                else if (b < 0)
+                    b = 0;
+                hsi_Values[i * 3] = (byte)b;
+                hsi_Values[i * 3 + 1] = (byte)g;
+                hsi_Values[i * 3 + 2] = (byte)r;
+                count_eq[eqHist[hsi_index]]++;
+            }
+            //System.Runtime.InteropServices.Marshal.Copy(hsi_Ptr, hsi_Values, 0, hsi_bytes);//複製GRB信息到byte數組
+            System.Runtime.InteropServices.Marshal.Copy(hsi_Values, 0, hsi_Ptr, hsi_bytes);
+            chart1.Series["Series1"].LegendText = "eq";
+            chart1.ChartAreas["ChartArea1"].AxisX.Minimum = 0;
+            chart1.ChartAreas["ChartArea1"].AxisX.Maximum = 255;
+            for (i = 0; i <= 255; i++)
+            {
+                chart1.Series["Series1"].Points.AddXY(i, count_eq[i]);
+            }
+
+            hsi_image.UnlockBits(hsi_data);
+            ori_image.UnlockBits(ori_data);
+            pictureBox1.Image = hsi_image;
+        }
+        public void RGB2HSI()
+        {
+
+        }
+
+    }
+}
