7e67f756b6eb7e2120eb974d0ad5e1a4de473c47
linyon <asd357076@gmail.com>
2017-05-15 19:43:11 +0800

    2017-0515-update hist_shrinking

--- Exercise/20170509-RGB_2_HSI_HistogramEqualization/20170509-RGB_2_HSI_HistogramEqualization/Form1.Designer.cs
+++ Exercise/20170509-RGB_2_HSI_HistogramEqualization/20170509-RGB_2_HSI_HistogramEqualization/Form1.Designer.cs
@@ -34,44 +34,29 @@
             System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea4 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
             System.Windows.Forms.DataVisualization.Charting.Legend legend4 = new System.Windows.Forms.DataVisualization.Charting.Legend();
             System.Windows.Forms.DataVisualization.Charting.Series series4 = new System.Windows.Forms.DataVisualization.Charting.Series();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
             this.pictureBox2 = new System.Windows.Forms.PictureBox();
             this.chart2 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.開檔ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.開檔ToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.直方圖ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.均衡化ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.縮小ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.擴展ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.chart2)).BeginInit();
+            this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(22, 23);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(103, 52);
-            this.button1.TabIndex = 2;
-            this.button1.Text = "開圖";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
-            // button2
-            // 
-            this.button2.Cursor = System.Windows.Forms.Cursors.Default;
-            this.button2.Location = new System.Drawing.Point(22, 93);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(103, 52);
-            this.button2.TabIndex = 3;
-            this.button2.Text = "均衡化";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // pictureBox1
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(150, 23);
+            this.pictureBox1.Location = new System.Drawing.Point(12, 30);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(411, 299);
+            this.pictureBox1.Size = new System.Drawing.Size(494, 376);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox1.TabIndex = 4;
             this.pictureBox1.TabStop = false;
@@ -82,21 +67,21 @@
             this.chart1.ChartAreas.Add(chartArea3);
             legend3.Name = "Legend1";
             this.chart1.Legends.Add(legend3);
-            this.chart1.Location = new System.Drawing.Point(28, 328);
+            this.chart1.Location = new System.Drawing.Point(12, 428);
             this.chart1.Name = "chart1";
             series3.ChartArea = "ChartArea1";
             series3.Legend = "Legend1";
             series3.Name = "Series1";
             this.chart1.Series.Add(series3);
-            this.chart1.Size = new System.Drawing.Size(533, 279);
+            this.chart1.Size = new System.Drawing.Size(494, 381);
             this.chart1.TabIndex = 5;
             this.chart1.Text = "chart1";
             // 
             // pictureBox2
             // 
-            this.pictureBox2.Location = new System.Drawing.Point(567, 23);
+            this.pictureBox2.Location = new System.Drawing.Point(512, 30);
             this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(411, 299);
+            this.pictureBox2.Size = new System.Drawing.Size(494, 376);
             this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox2.TabIndex = 6;
             this.pictureBox2.TabStop = false;
@@ -107,45 +92,110 @@
             this.chart2.ChartAreas.Add(chartArea4);
             legend4.Name = "Legend1";
             this.chart2.Legends.Add(legend4);
-            this.chart2.Location = new System.Drawing.Point(567, 328);
+            this.chart2.Location = new System.Drawing.Point(512, 428);
             this.chart2.Name = "chart2";
             series4.ChartArea = "ChartArea1";
             series4.Legend = "Legend1";
             series4.Name = "Series1";
             this.chart2.Series.Add(series4);
-            this.chart2.Size = new System.Drawing.Size(533, 279);
+            this.chart2.Size = new System.Drawing.Size(494, 381);
             this.chart2.TabIndex = 7;
             this.chart2.Text = "chart2";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.開檔ToolStripMenuItem,
+            this.直方圖ToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1243, 28);
+            this.menuStrip1.TabIndex = 8;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // 開檔ToolStripMenuItem
+            // 
+            this.開檔ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.開檔ToolStripMenuItem1});
+            this.開檔ToolStripMenuItem.Name = "開檔ToolStripMenuItem";
+            this.開檔ToolStripMenuItem.Size = new System.Drawing.Size(51, 24);
+            this.開檔ToolStripMenuItem.Text = "選項";
+            // 
+            // 開檔ToolStripMenuItem1
+            // 
+            this.開檔ToolStripMenuItem1.Name = "開檔ToolStripMenuItem1";
+            this.開檔ToolStripMenuItem1.Size = new System.Drawing.Size(114, 26);
+            this.開檔ToolStripMenuItem1.Text = "開檔";
+            this.開檔ToolStripMenuItem1.Click += new System.EventHandler(this.開檔ToolStripMenuItem1_Click);
+            // 
+            // 直方圖ToolStripMenuItem
+            // 
+            this.直方圖ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.均衡化ToolStripMenuItem,
+            this.縮小ToolStripMenuItem,
+            this.擴展ToolStripMenuItem});
+            this.直方圖ToolStripMenuItem.Name = "直方圖ToolStripMenuItem";
+            this.直方圖ToolStripMenuItem.Size = new System.Drawing.Size(66, 24);
+            this.直方圖ToolStripMenuItem.Text = "直方圖";
+            this.直方圖ToolStripMenuItem.Click += new System.EventHandler(this.直方圖ToolStripMenuItem_Click);
+            // 
+            // 均衡化ToolStripMenuItem
+            // 
+            this.均衡化ToolStripMenuItem.Name = "均衡化ToolStripMenuItem";
+            this.均衡化ToolStripMenuItem.Size = new System.Drawing.Size(181, 26);
+            this.均衡化ToolStripMenuItem.Text = "均衡化";
+            this.均衡化ToolStripMenuItem.Click += new System.EventHandler(this.均衡化ToolStripMenuItem_Click);
+            // 
+            // 縮小ToolStripMenuItem
+            // 
+            this.縮小ToolStripMenuItem.Name = "縮小ToolStripMenuItem";
+            this.縮小ToolStripMenuItem.Size = new System.Drawing.Size(181, 26);
+            this.縮小ToolStripMenuItem.Text = "縮小";
+            this.縮小ToolStripMenuItem.Click += new System.EventHandler(this.縮小ToolStripMenuItem_Click);
+            // 
+            // 擴展ToolStripMenuItem
+            // 
+            this.擴展ToolStripMenuItem.Name = "擴展ToolStripMenuItem";
+            this.擴展ToolStripMenuItem.Size = new System.Drawing.Size(181, 26);
+            this.擴展ToolStripMenuItem.Text = "擴展";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1159, 619);
+            this.ClientSize = new System.Drawing.Size(1243, 821);
             this.Controls.Add(this.chart2);
             this.Controls.Add(this.pictureBox2);
             this.Controls.Add(this.chart1);
             this.Controls.Add(this.pictureBox1);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
             this.Name = "Form1";
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.chart2)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
         private System.Windows.Forms.PictureBox pictureBox2;
         private System.Windows.Forms.DataVisualization.Charting.Chart chart2;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem 開檔ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 直方圖ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 均衡化ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 開檔ToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem 縮小ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 擴展ToolStripMenuItem;
     }
 }
 
--- Exercise/20170509-RGB_2_HSI_HistogramEqualization/20170509-RGB_2_HSI_HistogramEqualization/Form1.cs
+++ Exercise/20170509-RGB_2_HSI_HistogramEqualization/20170509-RGB_2_HSI_HistogramEqualization/Form1.cs
@@ -18,41 +18,49 @@ namespace _20170509_RGB_2_HSI_HistogramEqualization
             InitializeComponent();
         }
         Bitmap ori_image;
+        BitmapData ori_data;
+        Rectangle imageRect;
+        Bitmap hsi_image;
+        BitmapData hsi_data;
+        List<int> hsi_h = new List<int>();
+        List<double> hsi_s = new List<double>();
+        List<double> hsi_i = new List<double>();
+        byte[] ori_Values;
+        int ori_bytes;
+        byte[] hsi_Values;
+        int hsi_bytes;
+        int[] count = new int[256];
+
+
         private void button1_Click(object sender, EventArgs e)
         {
-            OpenFileDialog ofd = new OpenFileDialog();
-            ofd.Filter = "Image Files(*.BMP;*.JPG;*.GIF;*.PNG)|*.BMP;*.JPG;*.GIF;*.PNG";
-            if (ofd.ShowDialog() == DialogResult.OK)
-            {
-                pictureBox1.Image = Image.FromFile(ofd.FileName);
-            }
-            ori_image = pictureBox1.Image as Bitmap;
-            pictureBox1.Image = ori_image;
+
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+
         }
 
-        private void button2_Click(object sender, EventArgs e)
+        private void 均衡化ToolStripMenuItem_Click(object sender, EventArgs e)
         {
             int w = ori_image.Width;
             int h = ori_image.Height;
-            var imageRect = new Rectangle(0, 0, w, h);
-            var ori_data = ori_image.LockBits(imageRect, ImageLockMode.ReadWrite, ori_image.PixelFormat);
-            var hsi_image = new Bitmap(imageRect.Width, imageRect.Height);
-            var hsi_data = hsi_image.LockBits(imageRect, ImageLockMode.ReadWrite, ori_image.PixelFormat);
 
-            System.IntPtr ori_Ptr = ori_data.Scan0;
+            //ori_data = ori_image.LockBits(imageRect, ImageLockMode.ReadWrite, ori_image.PixelFormat);
+            hsi_data = hsi_image.LockBits(imageRect, ImageLockMode.ReadWrite, ori_image.PixelFormat);
+
+            //System.IntPtr ori_Ptr = ori_data.Scan0;
             System.IntPtr hsi_Ptr = hsi_data.Scan0;
 
-            int ori_bytes = ori_data.Stride * h;
-            int hsi_bytes = hsi_data.Stride * h;
-            byte[] ori_Values = new byte[ori_bytes];
-            byte[] hsi_Values = new byte[hsi_bytes];
-            System.Runtime.InteropServices.Marshal.Copy(ori_Ptr, ori_Values, 0, ori_bytes);//複製GRB信息到byte數組
+            //ori_bytes = ori_data.Stride * h;
+            //int hsi_bytes = hsi_data.Stride * h;
+            //ori_Values = new byte[ori_bytes];
+            //hsi_Values = new byte[hsi_bytes];
+            //System.Runtime.InteropServices.Marshal.Copy(ori_Ptr, ori_Values, 0, ori_bytes);//複製GRB信息到byte數組
             System.Runtime.InteropServices.Marshal.Copy(hsi_Ptr, hsi_Values, 0, hsi_bytes);//複製GRB信息到byte數組
-            var hsi_h = new List<int>();
-            var hsi_s = new List<double>();
-            var hsi_i = new List<double>();
-            var count = new int[256];
-            Method.RGB2HSI(ori_Values, h, w, ori_data.Stride, hsi_h, hsi_s, hsi_i, count); // RGB 轉 HSI
+
+            //var count = new int[256];
+            //Method.RGB2HSI(ori_Values, h, w, ori_data.Stride, hsi_h, hsi_s, hsi_i, count); // RGB 轉 HSI
             var eqHist = new int[256];
             var count_eq = new int[256];
             int size = ori_bytes / 3;
@@ -60,13 +68,7 @@ namespace _20170509_RGB_2_HSI_HistogramEqualization
             Method.HSI2RGB(hsi_Values, eqHist, size, hsi_h, hsi_s, hsi_i, count_eq);
             System.Runtime.InteropServices.Marshal.Copy(hsi_Values, 0, hsi_Ptr, hsi_bytes);
 
-            chart1.Series["Series1"].LegendText = "原圖的I";
-            chart1.ChartAreas["ChartArea1"].AxisX.Minimum = 0;
-            chart1.ChartAreas["ChartArea1"].AxisX.Maximum = 255;
-            for (int i = 0; i <= 255; i++)
-            {
-                chart1.Series["Series1"].Points.AddXY(i, count[i]);
-            }
+
             chart2.Series["Series1"].LegendText = "I均衡化後";
             chart2.ChartAreas["ChartArea1"].AxisX.Minimum = 0;
             chart2.ChartAreas["ChartArea1"].AxisX.Maximum = 255;
@@ -75,10 +77,103 @@ namespace _20170509_RGB_2_HSI_HistogramEqualization
                 chart2.Series["Series1"].Points.AddXY(i, count_eq[i]);
             }
             hsi_image.UnlockBits(hsi_data);
-            ori_image.UnlockBits(ori_data);
+            //ori_image.UnlockBits(ori_data);
             pictureBox1.Image = ori_image;
             pictureBox2.Image = hsi_image;
         }
-        
+
+        private void 開檔ToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "Image Files(*.BMP;*.JPG;*.GIF;*.PNG)|*.BMP;*.JPG;*.GIF;*.PNG";
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                pictureBox1.Image = Image.FromFile(ofd.FileName);
+                hsi_h.Clear();
+                hsi_s.Clear();
+                hsi_i.Clear();
+            }
+            ori_image = pictureBox1.Image as Bitmap;
+            pictureBox1.Image = ori_image;
+
+            imageRect = new Rectangle(0, 0, ori_image.Width, ori_image.Height);
+            ori_data = ori_image.LockBits(imageRect, ImageLockMode.ReadWrite, ori_image.PixelFormat);
+            hsi_image = new Bitmap(imageRect.Width, imageRect.Height);
+            hsi_data = hsi_image.LockBits(imageRect, ImageLockMode.ReadWrite, ori_image.PixelFormat);
+            System.IntPtr ori_Ptr = ori_data.Scan0;
+
+            ori_bytes = ori_data.Stride * ori_image.Height;
+            hsi_bytes = hsi_data.Stride * ori_image.Height;
+            ori_Values = new byte[ori_bytes];
+            hsi_Values = new byte[hsi_bytes];
+            System.Runtime.InteropServices.Marshal.Copy(ori_Ptr, ori_Values, 0, ori_bytes);//複製GRB信息到byte數組
+            Method.RGB2HSI(ori_Values, ori_image.Height, ori_image.Width, ori_data.Stride, hsi_h, hsi_s, hsi_i, count); // RGB 轉 HSI
+            hsi_i.Count();
+            ori_image.UnlockBits(ori_data);
+            hsi_image.UnlockBits(hsi_data);
+            chart1.Series["Series1"].LegendText = "原圖的I";
+            chart1.ChartAreas["ChartArea1"].AxisX.Minimum = 0;
+            chart1.ChartAreas["ChartArea1"].AxisX.Maximum = 255;
+            for (int i = 0; i <= 255; i++)
+            {
+                chart1.Series["Series1"].Points.AddXY(i, count[i]);
+            }
+        }
+        private void 直方圖ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void 縮小ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            hsi_data = hsi_image.LockBits(imageRect, ImageLockMode.ReadWrite, ori_image.PixelFormat);
+            System.IntPtr hsi_Ptr = hsi_data.Scan0;
+            System.Runtime.InteropServices.Marshal.Copy(hsi_Ptr, hsi_Values, 0, hsi_bytes);
+            double r_max = 0;
+            double r_min = 255;
+            double s_max = 255;
+            double s_min = 155;
+            double r, tmp;
+            int size = ori_bytes / 3;
+            var count_shrinking = new int[256];
+            var shrHist = new int[256];
+            hsi_h.Count();
+            var hist_shrink = new List<double>();
+            for (int i = 0; i < size; i++)
+            {
+                tmp = hsi_i[i] * 255.0;
+                if (tmp > r_max) r_max = tmp;
+                if (tmp < r_min) r_min = tmp;
+            }
+            for (int i = 0; i < 256; i++)
+            {
+                shrHist[i] = i;
+                count_shrinking[i] = 0;
+            }
+            for (int i = 0; i < size; i++)
+            {
+                r = hsi_i[i]*255;
+                if ((r_max - r_min) == 0)
+                {
+                    hist_shrink[i] = s_min;
+                }
+                else
+                {
+                    tmp = (s_max - s_min) * (r - r_min) / (r_max - r_min) + s_min;
+                    hist_shrink.Add(tmp/255);//(r-r_min)
+                }//count_shrinking[(int)hist_shrink[index]]++;
+            }
+
+            Method.HSI2RGB(hsi_Values, shrHist, size, hsi_h, hsi_s, hist_shrink, count_shrinking);
+            System.Runtime.InteropServices.Marshal.Copy(hsi_Values, 0, hsi_Ptr, hsi_bytes);
+            chart2.Series["Series1"].LegendText = "I縮小後";
+            chart2.ChartAreas["ChartArea1"].AxisX.Minimum = 0;
+            chart2.ChartAreas["ChartArea1"].AxisX.Maximum = 255;
+            for (int i = 0; i <= 255; i++)
+            {
+                chart2.Series["Series1"].Points.AddXY(i, count_shrinking[i]);
+            }
+            hsi_image.UnlockBits(hsi_data);
+            pictureBox2.Image = hsi_image;
+        }
     }
 }
